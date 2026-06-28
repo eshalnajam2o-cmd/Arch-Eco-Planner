1fb2f72446e4456437bc1dcdf7490cc7ade2bb6c
eshalnajam2o-cmd <eshalnajam.2.o@gmail.com>
2026-06-28 16:25:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import requests
 # --- LOGO DESIGN ---
-# Using a secure direct web link ensures your logo loads perfectly without file errors!
-LOGO_URL = "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=800&q=80"
+# Using a secure direct web link ensures your logo loads perfectly without file errors
 
-st.image(LOGO_URL, use_container_width=True)
+st.image("logo.png", use_container_width=True)
 # 1. Page Title and Styling
 st.set_page_config(page_title="Arch-Eco Spatial Planner", layout="centered")
 st.title(" Arch-Eco Contextual Spatial Planner")
